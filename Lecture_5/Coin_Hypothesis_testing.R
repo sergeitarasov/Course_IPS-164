ac3669ea30d8550b7a481ff50ff0879abd520c42
Sergei Tarasov <sergxf@yandex.ru>
2020-11-05 12:01:41 +0200

    Create Coin_Hypothesis_testing.R
    1

---
+++ Lecture_5/Coin_Hypothesis_testing.R
@@ -0,0 +1,11 @@
+# 
+#   ____________________________________________________________________________
+#   Testing Hypothesis if coin is fair or not using Maximum Likelihood      ####
+
+# Simulating data
+
+heads1=rbinom(1, 100, prob= )
+heads2=rbinom(1, 100, prob= )
+
+#-------------------------
+# Testing coin1
